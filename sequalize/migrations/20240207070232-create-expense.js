985b0d04c102b95c0590fb2bd2065ac644e3677e
queenslaynaila <queenslayjema@gmail.com>
2024-02-08 04:22:59 +0300

    feat(migrations): Add migration file for creating expenses table

---
+++ sequalize/migrations/20240207070232-create-expense.js
@@ -0,0 +1,43 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  up:(queryInterface, Sequelize)=>{
+    return queryInterface.createTable('Expenses', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      amount: {
+        type: Sequelize.DECIMAL
+      },
+      category: {
+        type: Sequelize.STRING
+      },
+      date: {
+        type: Sequelize.DATE
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        onDelete: 'CASCADE',
+        references: {
+          model: 'Users',
+          key: 'id',
+          as: 'userId',
+        }
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+   down:(queryInterface, Sequelize)=>{
+    return queryInterface.dropTable('Expenses');
+  }
+};
